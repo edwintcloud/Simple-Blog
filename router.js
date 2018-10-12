2ba4b21810b898a1b1b14b550934b94d70beb5b2
Edwin Cloud <ecloud412@gmail.com>
2018-10-11 19:49:05 -0700

    Base design work

--- router.js
+++ router.js
@@ -3,6 +3,12 @@ const router = require('express').Router()
 // import our controllers
 import posts from './controllers/posts'
 
+// router middleware that will be run on all requests
+router.use((req, res, next) => {
+    res.locals.blogName = "My Blog"
+    next()
+})
+
 // posts routes
 router.get('/', posts.get)
 
