0dddab87363a953f0391b7846cce5c08c3a8665a
Edwin Cloud <ecloud412@gmail.com>
2018-10-12 18:15:34 -0700

    Navbar and footer done

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,25 @@
 // import our model
 import Post from '../models/post'
+import Comment from '../models/comment'
 
 // get all our post and render the index
-exports.get = (req, res) => {
-    res.render('index')
+exports.get = async (req, res) => {
+    try {
+        const posts = await Post.find()
+        const comments = await Comment.find()
+        res.render('index', { posts: posts, comments: comments })
+    } catch(e) {
+        return console.error(e.message)
+    }
+
+}
+
+// create a new post
+exports.new = (req, res) => {
+    Post.create(req.body).then((post) => {
+        res.status(200).send(post)
+    }).catch((e) => {
+        res.status(400).send(e.message)
+        console.error(e.message)
+    })
 }
